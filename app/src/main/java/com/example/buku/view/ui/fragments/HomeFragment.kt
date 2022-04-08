74305dc30aadf22bab9b99c59cad8f1360f8ebf9
Liliana Gomez <gomez.liliana333@gmail.com>
2022-04-07 22:40:30 -0400

    Adding material design for log in layout

--- app/src/main/java/com/example/buku/view/ui/fragments/HomeFragment.kt
+++ app/src/main/java/com/example/buku/view/ui/fragments/HomeFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.navigation.fragment.findNavController
 import com.example.buku.R
 
 // TODO: Rename parameter arguments, choose names that match
@@ -24,6 +26,7 @@ class HomeFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         arguments?.let {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
