393ef0b13c37526b8208707ae84e0b798e19511d
Liliana Gomez <gomez.liliana333@gmail.com>
2022-04-19 21:55:20 -0400

    Setting reading from json with gson

--- app/src/main/java/com/example/buku/model/Book.kt
+++ app/src/main/java/com/example/buku/model/Book.kt
@@ -1,14 +1,28 @@
 package com.example.buku.model
 
-data class Book(
-    var name: String,
-    var author: String,
-    var location: String,
-    var imageUrl: String,
-//    var score: Int = 0,
-//    var available: Boolean = true,
+import com.google.gson.annotations.SerializedName
 
+data class Book(
+    @SerializedName("name")
+    val name: String,
+    @SerializedName("author")
+    val author: String,
+    @SerializedName("location")
+    val location: String,
+    @SerializedName("imageUrl")
+    val imageUrl: String
 )
+
+//
+//data class Book(
+//    var name: String,
+//    var author: String,
+//    var location: String,
+//    var imageUrl: String,
+////    var score: Int = 0,
+////    var available: Boolean = true,
+//
+//)
 
 //    fun bookToLowerCase(): String{
 //        return name.toLowerCase()
--- app/src/main/java/com/example/buku/view/ui/fragments/HomeFragment.kt
+++ app/src/main/java/com/example/buku/view/ui/fragments/HomeFragment.kt
@@ -1,6 +1,7 @@
 package com.example.buku.view.ui.fragments
 
 import android.os.Bundle
+
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -9,10 +10,12 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.buku.R
 import com.example.buku.model.Book
+import com.example.buku.model.BookList
 import com.example.buku.model.Category
 import com.example.buku.view.adapter.BooksAdapter
 import com.example.buku.view.adapter.CategoriesAdapter
 import com.google.gson.Gson
+
 
 
 class HomeFragment : Fragment() {
@@ -38,7 +41,6 @@ class HomeFragment : Fragment() {
         super.onViewCreated(itemView, savedInstanceState)
 
         booksRecyclerView = requireView().findViewById(R.id.rvBooksHome)
-//        listBooks = createMockBooks()
         listBooks = loadMockBooksFromJason()
         booksAdapter = BooksAdapter(listBooks)
 
@@ -61,42 +63,11 @@ class HomeFragment : Fragment() {
 
     private fun loadMockBooksFromJason(): ArrayList<Book> {
         val booksString: String =
-            activity.applicationContext.assets.open("books.json").bufferedReader()
-                .use { it.readText() }
+            activity?.applicationContext?.assets?.open("books.json")?.bufferedReader()
+                .use { it!!.readText() }
         val gson = Gson()
-        val data = gson.fromJson(booksString, Book::class.java)
-        return data
+        return gson.fromJson(booksString, BookList::class.java)
 
-    }
-
-
-    private fun createMockBooks(): ArrayList<Book> {
-        return arrayListOf(
-            Book(
-                name = "Will",
-                author = "Will Smith with Mark Manson",
-                location = "Bogota",
-                imageUrl = "https://markmanson.net/wp-content/uploads/2021/11/will-book-cover.png"
-            ),
-            Book(
-                name = "One Hundred Years of Solitude",
-                author = "Gabriel Garcia Marquez",
-                location = "Suba",
-                imageUrl = "https://www.jmichaelsbooks.com/pictures/900662.jpg"
-            ),
-            Book(
-                name = "Eat Pray Love",
-                author = "Elizabeth Gilbert",
-                location = "Cali",
-                imageUrl = "https://images.squarespace-cdn.com/content/v1/56b1196f37013bf2902300c6/1550101007941-GNR0VNCUICFWVV8C2MEY/IMG_8767.jpg"
-            ),
-            Book(
-                name = "The Subtle Art of Not Giving a F*ck",
-                author = "EMark Manson",
-                location = "Bogota",
-                imageUrl = "https://miro.medium.com/max/1400/1*qXp9MemqNmYwhoNptxeflg.jpeg"
-            ),
-        )
     }
 
 
